297b599ffee5239bb957d57fce7fce1f98c55e99
yuanboli233 <yuanboli233@gmail.com>
2020-07-25 20:42:11 -0400

    support files

---
+++ debugger/issue/bug1.c
@@ -0,0 +1,10 @@
+#include "csmith.h"
+volatile uint32_t csmith_sink_ = 0;
+static int16_t g_275[1] = {4};
+int main (void)
+{
+    int i, j;
+    {
+        csmith_sink_ = g_275[i];
+    }
+}
---
+++ debugger/issue/bug2.c
@@ -0,0 +1,13 @@
+#include "csmith.h"
+static int32_t g_10[7] = {4,1,4,4,1,4,4};
+static int32_t *g_66[1][3][2] = {{{&g_10[1],&g_10[1]},{&g_10[1],&g_10[1]},{&g_10[1],&g_10[1]}}};
+static int32_t ** volatile g_830 = &g_66[0][0][0];
+static uint32_t  func_1(void)
+{ 
+    int32_t *l_1280[9][4] = {{(void*)0,(void*)0,(void*)0,(void*)0},{(void*)0,(void*)0,(void*)0,(void*)0},{(void*)0,(void*)0,(void*)0,(void*)0},{(void*)0,(void*)0,(void*)0,(void*)0},{(void*)0,(void*)0,(void*)0,(void*)0},{(void*)0,(void*)0,(void*)0,(void*)0},{(void*)0,(void*)0,(void*)0,(void*)0},{(void*)0,(void*)0,(void*)0,(void*)0},{(void*)0,(void*)0,(void*)0,(void*)0}};
+    (*g_830) = l_1280[8][1];
+}
+int main (void)
+{
+    func_1();
+}
