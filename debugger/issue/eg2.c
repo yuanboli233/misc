ab4ac9c0b70b39f9467a45fc17ece16614fbca3d
yuanboli233 <yuanboli233@gmail.com>
2020-07-27 08:47:09 -0400

    remove header

--- debugger/issue/eg2.c
+++ debugger/issue/eg2.c
@@ -1,4 +1,7 @@
-#include "csmith.h"
+typedef unsigned int __uint32_t;
+typedef signed short int __int16_t;
+typedef __uint32_t uint32_t;
+typedef __int16_t int16_t;
 volatile uint32_t csmith_sink_ = 0;
 static int16_t g_275[1] = {4};
 int main (void)
